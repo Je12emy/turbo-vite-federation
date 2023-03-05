e5c3c5bccdedd4225b2f384f4f87e93991852d7f
Jeremy <jeremy@je12emy.com>
2023-03-05 13:53:43 -0600

    fix: add css file to App.tsx

--- remote/src/App.tsx
+++ remote/src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="bg-white shadow-md p-4 rounded">
-      <h1 className="text-lg"> I'm a remote React app using Tailwind </h1>
+      <h1 className="text-lg"> I'm a remote React app using Tailwind CSS </h1>
       <p className="text-gray-600">Count: {count}</p>
       <button
         className="bg-gray-800 text-white p-2"
